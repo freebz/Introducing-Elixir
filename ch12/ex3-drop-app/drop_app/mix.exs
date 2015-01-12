645d59d06e304585cc607780922d679ea66763d6
FreeBz <freebz.dev@gmail.com>
2015-01-12 11:10:01 +0900

    Chapter 12 Getting Started with OTP

---
+++ ch12/ex3-drop-app/drop_app/mix.exs
@@ -0,0 +1,32 @@
+defmodule DropApp.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :drop_app,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: [:logger],
+		 registered: [:drop_app],
+		 mod: {DropApp, []} ]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    []
+  end
+end
